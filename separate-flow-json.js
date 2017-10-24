545c4eed374dd126d44cc25f3bc295139ccefd89
high-u <zen.crazyd@gmail.com>
2017-10-24 20:21:57 +0900

    require fs-extra

--- separate-flow-json.js
+++ separate-flow-json.js
@@ -1,6 +1,7 @@
 "use strict";
-var fs = require("fs");
-var mkdirp = require('mkdirp');
+//var fs = require("fs");
+const fse = require('fs-extra');
+//var mkdirp = require('mkdirp');
 
 module.exports = function (RED) {
   function sepfunctempNode(config) {
@@ -9,15 +10,19 @@ module.exports = function (RED) {
     var node = this;
     this.on('input', function (msg) {
       var dirname = RED.settings.userDir + "/" + RED.settings.flowFile.replace(/\./g, "-") + "-codes";
-      mkdirp(dirname, function (err) {
-        if (err) {
-          console.log(err);
-        } 
+
+      fse.removeSync(dirname);
+      fse.ensureDirSync(dirname);
+      //mkdirp(dirname, function (err) {
+      //  if (err) {
+      //    console.log(err);
+      //  } 
         var flowfilename = RED.settings.userDir + "/" + RED.settings.flowFile;
-        var flow = fs.readFileSync(flowfilename, 'utf-8');
+        //var flow = fs.readFileSync(flowfilename, 'utf-8');
+        var flow = fse.readJsonSync(flowfilename);
         divisioner(flow, RED.settings.flowFile, dirname);
-        return;
-      });
+      //  return;
+      //});
       msg.payload = dirname;
       node.send(msg);
     });
@@ -26,13 +31,18 @@ module.exports = function (RED) {
 };
 
 var divisioner = function (json, outputname, outputdir) {
+  
   var specialKeys = { func: ".js", template: ".txt" };
   var arrObj = [];
-  var jsonObject = JSON.parse(json);
+  //var jsonObject = JSON.parse(json);
+  var jsonObject = json;
+
   // node loop
   jsonObject.forEach(function (noderedNode) {
+    console.log("01");
     // key loop
     Object.keys(noderedNode).forEach(function (key) {
+      console.log("02");
       if (specialKeys.hasOwnProperty(key)) {
         arrObj.push({
           id: this.id,
@@ -46,27 +56,33 @@ var divisioner = function (json, outputname, outputdir) {
     }, noderedNode);
     //arrObj.push({ id: noderedNode.id, type: noderedNode.type, key: "_node", value: JSON.stringify(noderedNode, null, "  "), ext: ".json" });
   });
-
-  mkdirp(outputdir + "/codes", function (err) {
-    if (err) {
-      console.log(err);
-    } else {
+  console.log("03");
+  fse.ensureDirSync(outputdir + "/codes");
+  //mkdirp(outputdir + "/codes", function (err) {
+  //  if (err) {
+  //    console.log(err);
+  //  } else {
       // write flow.json
-      fs.writeFile(outputdir + "/" + outputname, JSON.stringify(jsonObject, undefined, "  "), function (err) {
-        if (err) {
-          console.log(err);
-        }
-      });
+      //fs.writeFile(outputdir + "/" + outputname, JSON.stringify(jsonObject, undefined, "  "), function (err) {
+      //  if (err) {
+      //    console.log(err);
+      //  }
+      //});
+      fse.outputFileSync(outputdir + "/" + outputname, JSON.stringify(jsonObject, undefined, "  "));
+  console.log("04");
       // write children node
       arrObj.forEach(function (obj) {
         var nodefilepath = outputdir + "/codes/" + obj.type + "-" + obj.key + "-" + obj.id + obj.ext;
         
-        fs.writeFile(nodefilepath, obj.value, function (err) {
-          if (err) {
-            console.log(err);
-          }
-        });
+        //fs.writeFile(nodefilepath, obj.value, function (err) {
+        //  if (err) {
+        //    console.log(err);
+        //  }
+        //});
+        fse.outputFileSync(nodefilepath, obj.value);
+
       });
-    }
-  }); 
+  //  }
+  //}); 
+      return;
 };
