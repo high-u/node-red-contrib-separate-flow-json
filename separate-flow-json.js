8e3c7a7c9f3a3146a800bf8f2a534ea629c4cc60
high-u <zen.crazyd@gmail.com>
2017-10-24 13:10:03 +0900

    little refactoring

--- separate-flow-json.js
+++ separate-flow-json.js
@@ -2,92 +2,31 @@
 var fs = require("fs");
 var mkdirp = require('mkdirp');
 
-
 module.exports = function (RED) {
   function sepfunctempNode(config) {
     RED.nodes.createNode(this, config);
     this.target = config.target;
     var node = this;
     this.on('input', function (msg) {
-
       var dirname = RED.settings.userDir + "/" + RED.settings.flowFile.replace(/\./g, "-") + "-codes";
-      console.log(dirname);
-
       mkdirp(dirname, function (err) {
         if (err) {
           console.log(err);
         } 
         var flowfilename = RED.settings.userDir + "/" + RED.settings.flowFile;
         var flow = fs.readFileSync(flowfilename, 'utf-8');
-
-        //var json = JSON.stringify(divisioner(flow, dirname, "flow.json"));
-        //fs.writeFileSync(dirname + "/test.txt", json);
         divisioner(flow, RED.settings.flowFile, dirname);
-        
         return;
       });
-
-      
-
-      /*
-      RED.settings.flowFile
-
-      flows.json
-      */
-
-      /*
-      RED.settings.userDir
-
-      /Users/higuchi/Projects/Node-RED/node-red/.node-red
-      */
-
-      /*
-      RED.settings
-
-      { get: [Function: get],
-      set: [Function: set],
-      delete: [Function: delete],
-      available: [Function: available],
-      registerNodeSettings: [Function: registerNodeSettings],
-      exportNodeSettings: [Function: exportNodeSettings],
-      enableNodeSettings: [Function: enableNodeSettings],
-      disableNodeSettings: [Function: disableNodeSettings],
-      uiPort: [Getter/Setter],
-      mqttReconnectTime: [Getter/Setter],
-      serialReconnectTime: [Getter/Setter],
-      debugMaxLength: [Getter/Setter],
-      flowFile: [Getter/Setter],
-      functionGlobalContext: [Getter/Setter],
-      logging: [Getter/Setter],
-      settingsFile: [Getter/Setter],
-      httpRoot: [Getter/Setter],
-      disableEditor: [Getter/Setter],
-      httpAdminRoot: [Getter/Setter],
-      httpAdminAuth: [Getter/Setter],
-      httpNodeRoot: [Getter/Setter],
-      httpNodeAuth: [Getter/Setter],
-      uiHost: [Getter/Setter],
-      userDir: [Getter/Setter],
-      coreNodesDir: [Getter/Setter],
-      version: [Getter/Setter] }
-      */
-
-      
-
-      msg[node.target] = Number(msg[node.target]) + 1;
       node.send(msg);
     });
   }
   RED.nodes.registerType('separate-flow-json', sepfunctempNode);
 };
 
-
-
 var divisioner = function (json, outputname, outputdir) {
-  
   var specialKeys = { func: ".js", template: ".txt" };
   var arrObj = [];
-
   var jsonObject = JSON.parse(json);
   // node loop
   jsonObject.forEach(function (noderedNode) {
@@ -104,11 +43,9 @@ var divisioner = function (json, outputname, outputdir) {
         this[key] = "";
       }
     }, noderedNode);
-
     //arrObj.push({ id: noderedNode.id, type: noderedNode.type, key: "_node", value: JSON.stringify(noderedNode, null, "  "), ext: ".json" });
   });
 
-  
   mkdirp(outputdir + "/codes", function (err) {
     if (err) {
       console.log(err);
@@ -128,7 +65,6 @@ var divisioner = function (json, outputname, outputdir) {
             console.log(err);
           }
         });
-      
       });
     }
   }); 
