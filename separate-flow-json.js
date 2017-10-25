93889560dd84db74c9905fc3a59f32fa3c5365fb
high-u <zen.crazyd@gmail.com>
2017-10-25 10:22:53 +0900

    version 0.1.0. add comments.

--- separate-flow-json.js
+++ separate-flow-json.js
@@ -6,21 +6,27 @@ module.exports = function (RED) {
     RED.nodes.createNode(this, config);
     this.target = config.target;
     var node = this;
+    // on
     this.on('input', function (msg) {
+      // make path
       var dirname = RED.settings.userDir + "/" + RED.settings.flowFile.replace(/\./g, "-") + "-codes";
       fse.removeSync(dirname);
       fse.ensureDirSync(dirname);
+      // read flow json
       var flowfilename = RED.settings.userDir + "/" + RED.settings.flowFile;
       var flow = fse.readJsonSync(flowfilename);
-      divisioner(flow, RED.settings.flowFile, dirname);
+      // separate & save
+      separateFlowJson(flow, RED.settings.flowFile, dirname);
+      // return path
       msg.payload = dirname;
       node.send(msg);
     });
   }
+  // register node
   RED.nodes.registerType('separate-flow-json', sepfunctempNode);
 };
 
-var divisioner = function (json, outputname, outputdir) {
+var separateFlowJson = function (json, outputname, outputdir) {
   var specialKeys = { func: ".js", template: ".txt" };
   var arrObj = [];
   var jsonObject = json;
